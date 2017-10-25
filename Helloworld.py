a3e8dfa2dfb1020f31bd4dbc3c43994f03d31038
admin Jenkins <root@jenkinsmaster.sgt.com>
2017-10-25 05:30:25 -0400

    add existing file

--- Helloworld.py
+++ Helloworld.py
@@ -1,4 +1,8 @@
-#import Helloworld
+import os
+import sys
+import math
+
+print(os.getcwd())
 print ("Hello world ")
 print ("This is my first devops hello world test program ")
 print ("Thanks, Gangireddy  ")
@@ -13,4 +17,10 @@ print ("Thanks, Gangireddy  ")
 print ("Thanks, Gangireddy  ")
 print ("Thanks, Gangireddy  ")
 print ("Thanks, Gangireddy  ")
-print ("Thanks, Gangireddy  ")
+print ("Thanks, Gangireddy test ")
+print (sys.platform)
+print(math.sqrt(1001))
+x="blah";
+print x*3;
+input()
+
